eda3672b4da9fc8599e8db3476b9ba6e90c8b510
Arianna <ariigrangetto5@gmail.com>
2025-10-30 11:56:07 -0300

    feat: deleting comments

--- constrollers/users.js
+++ constrollers/users.js
@@ -8,9 +8,7 @@ export class ModelController {
   getUsers = async (req, res) => {
     const users = await this.userModel.getUsers();
     if (!users) {
-      return res
-        .status(500) //error del servidor
-        .json({ message: "internal service error" });
+      return res.status(500).json({ message: "internal service error" });
     }
     // res.render("users", {
     //   title: "Usuarios",
@@ -18,6 +16,7 @@ export class ModelController {
     // });
     return res.status(200).json({ message: "ok", users });
   };
+
   getUser = async (req, res) => {
     const { id } = req.params;
     const user = await this.userModel.getUser(id);
@@ -64,11 +63,13 @@ export class ModelController {
     }
 
     const updatedUser = await this.userModel.patchUser(id, result.data);
+
     if (!updatedUser) {
       res.status(404).json({
         message: "User cannot be modified",
       });
     }
+
     return res.status(200).json({ message: "ok", user: updatedUser });
   };
 
@@ -86,122 +87,3 @@ export class ModelController {
     return res.status(200).json({ message: "ok", user: userDeleted });
   };
 }
-
-// const getUsers = (req, res) => {
-//   api
-//     .getUsers()
-//     .then((users) => {
-//       res.status(201).json({ message: "ok", users });
-//     })
-//     .catch((err) => {
-//       res
-//         .status(500) //error del servidor
-//         .json({ message: "internal service error", error: err.message });
-//     });
-
-//   // res.render("users", {
-//   //   title: "Usuarios",
-//   //   users,
-//   // });
-// };
-
-// const getUser = (req, res) => {
-//   const { id } = req.params;
-//   api
-//     .getUser(id)
-//     .then((user) => {
-//       res.status(201).json({ message: "ok", user });
-//     })
-//     .catch((err) => {
-//       res.status(404).json({ message: "User not found", error: err.message });
-//     });
-//   // res.render("user", {
-//   //   title: "Usuario",
-//   //   user,
-//   // });
-// };
-
-// const getUserName = (req, res) => {
-//   const { firstName } = req.params;
-//   api
-//     .getUserName(firstName)
-//     .then((user) => {
-//       res.status(201).json({ message: "ok", user });
-//     })
-//     .catch((err) => {
-//       res.status(404).json({ message: "User not found", error: err.message });
-//     });
-
-//   // res.render("user", {
-//   //   title: firstName,
-//   //   user,
-//   // });
-// };
-
-// //Crear completamente un usuario
-// const postUser = (req, res) => {
-//   const result = validateUser(req.body);
-
-//   if (!result.success) {
-//     //bad request
-//     return res.status(400).json({ error: JSON.parse(result.error.message) });
-//   }
-
-//   api
-//     .createUser(result.data)
-//     .then((user) => {
-//       res.status(201).json({ message: "ok", user });
-//     })
-//     .catch((err) => {
-//       res
-//         .status(404)
-//         .json({ message: "User cannot be created", error: err.message });
-//     });
-//   // res.render("user", {
-//   //   title: "Nuevo usuario",
-//   //   user: newUser,
-//   // });
-// };
-
-// const patchUser = (req, res) => {
-//   const { id } = req.params;
-//   const result = validatePartialUser(req.body);
-
-//   if (!result.success) {
-//     //bad request
-//     return res.status(400).json({ error: JSON.parse(result.error.message) });
-//   }
-
-//   api
-//     .patchUser(id, result.data)
-//     .then((user) => {
-//       res.status(201).json({ message: "ok", user });
-//     })
-//     .catch((err) => {
-//       res.status(404).json({
-//         message: "User cannot be modified",
-//         error: err.message,
-//       });
-//     });
-
-//   // res.json({ message: "ok", user: updatedUser });
-//   // res.render("user", {
-//   //   title: "Usuario actualizado",
-//   //   user: updatedUser,
-//   // });
-// };
-
-// const deleteUser = (req, res) => {
-//   const { id } = req.params;
-//   api
-//     .deleteUser(id)
-//     .then((user) => {
-//       res.status(201).json({ message: "ok", user });
-//     })
-//     .catch((err) => {
-//       res.status(404).json({
-//         message: "User cannot be deleted",
-//         error: err.message,
-//       });
-//     });
-// };
